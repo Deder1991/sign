d7de35094b95e3663067cd2c3992822b98839609
Deder1991 <l22619025@gmail.com>
2020-05-07 16:44:21 +0800

    改寫法

--- sign.py
+++ sign.py
@@ -1,13 +1,15 @@
 counter = 3
 password = "a123456"
 while counter > 0 :
+	counter = counter - 1
 	pwd = input("input password:")
 	if pwd == password:
 		print("登入成功")
 		break
-	elif pwd != password and counter > 1 :
-		print("密碼錯誤! 還有", counter - 1, "次機會")
-
-	counter = counter - 1	
+	else :
+		print("密碼錯誤!")
+		if counter > 0:
+			print("還有", counter, "次機會")
+		
 
 	
